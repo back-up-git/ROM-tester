d446b3609e095b0d0cd64d21f8d903c46eb6427d
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-02-12 00:35:20 +0530

    Fix audio hals

--- sync.sh
+++ sync.sh
@@ -21,6 +21,8 @@ git clone --depth=1 $CM_LINK $CM_PATH
 
 rm -rf hardware/qcom-caf/sm8250/display
 rm -rf hardware/qcom-caf/sm8350/display
+rm -rf hardware/qcom-caf/sm8250/audio
+rm -rf hardware/qcom-caf/sm8350/audio
 #git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Exit
