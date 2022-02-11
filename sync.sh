0e01736c9cfb4260f33d18de549b87f454488988
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-02-12 00:10:55 +0530

    Final

--- sync.sh
+++ sync.sh
@@ -19,8 +19,9 @@ git clone --depth=1 $KT_LINK $KT_PATH
 git clone --depth=1 $HW_LINK $HW_PATH
 git clone --depth=1 $CM_LINK $CM_PATH
 
-rm -rf hardware/qcom-caf/sm8150/display
-git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8250/display
+rm -rf hardware/qcom-caf/sm8350/display
+#git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Exit
 exit 0
