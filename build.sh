070b6d17002987e88bc32a22e20699bca5db1f6f
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-02-27 18:29:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,16 +21,16 @@ lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
 
 #Never Allow Fix
 export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 
 # Build the Code
-if [ -z "$J_VAL" ]; then
-    make -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    make $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-else
-    make -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-fi
+/.rom-build.sh raphael
+#if [ -z "$J_VAL" ]; then
+#    make -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+#elif [ "$J_VAL"="0" ]; then
+#    make $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+#else
+#    make -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+#fi
 
 # Exit
 exit 0
