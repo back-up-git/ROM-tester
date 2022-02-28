9c183cc090db48f427b7dfca1ce4bbb7f8262b8c
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-02-28 19:52:44 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -19,16 +19,16 @@ git clone --depth=1 $KT_LINK $KT_PATH
 git clone --depth=1 $HW_LINK $HW_PATH
 git clone --depth=1 $CM_LINK $CM_PATH
 
-rm -rf hardware/qcom-caf/sm8150/display
-rm -rf hardware/qcom-caf/sm8250/display
-rm -rf hardware/qcom-caf/sm8350/display
-rm -rf hardware/qcom-caf/sm8150/audio
-rm -rf hardware/qcom-caf/sm8250/audio
-rm -rf hardware/qcom-caf/sm8350/audio
+#rm -rf hardware/qcom-caf/sm8150/display
+#rm -rf hardware/qcom-caf/sm8250/display
+#rm -rf hardware/qcom-caf/sm8350/display
+#rm -rf hardware/qcom-caf/sm8150/audio
+#rm -rf hardware/qcom-caf/sm8250/audio
+#rm -rf hardware/qcom-caf/sm8350/audio
 
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
-git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+#git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
+#git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+#git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-19.0 packages/resources/devicesettings
 
 # Exit
