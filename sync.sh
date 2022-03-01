ca277f8f958af337496499059893f77606dd82ac
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-01 19:16:59 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -12,7 +12,7 @@ repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
 # Sync the Sources
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
-# Clone Trees
+# Clone Trees #
 git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
