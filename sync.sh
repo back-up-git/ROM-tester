e3008257664cfa0eaa41176028bda4c56f05aea7
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-01 19:15:35 +0530

    Fix Build Error

--- sync.sh
+++ sync.sh
@@ -29,7 +29,7 @@ git clone --depth=1 $CM_LINK $CM_PATH
 #git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
 #git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 #git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
-#git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-19.0 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-19.0 packages/resources/devicesettings
 
 # Exit
 exit 0
