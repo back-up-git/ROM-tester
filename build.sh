b93233a661ee66e076506a4ec7febbca53382bbe
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-04 23:27:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,23 +13,10 @@ fi
 # Run the Extra Command
 # $EXTRA_CMD
 
-# Prepare the Build Environment
-source build/envsetup.sh
-
-# lunch the target
-lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-
 #Never Allow Fix
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-# Build the Code
-if [ -z "$J_VAL" ]; then
-    ./rom-build.sh DEVICE || { echo "ERROR: Build Failed!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    ./rom-build.sh DEVICE || { echo "ERROR: Build Failed!" && exit 1; }
-else
-    ./rom-build.sh DEVICE || { echo "ERROR: Build Failed!" && exit 1; }
-fi
+bash vendor/aospa/build.sh raphael
 
 # Exit
 exit 0
