b3fd1c46177f988b6cb3aadc8ece2c50de603247
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-05 03:11:51 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -16,7 +16,7 @@ repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 git clone --depth=1 $VT_LINK $VT_PATH
 git clone --depth=1 $KT_LINK $KT_PATH
-#git clone --depth=1 $HW_LINK $HW_PATH
+git clone --depth=1 $HW_LINK $HW_PATH
 rm -rf vendor/aospa
 git clone --depth=1 $VA_LINK $VA_PATH
 
