34f09b475ecab334d33effd24759aca89b443be8
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-05 02:23:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,16 +13,11 @@ fi
 # Run the Extra Command
 # $EXTRA_CMD
 
-# Prepare the Build Environment
-source build/envsetup.sh
-
-# lunch the target
-lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-
 #Never Allow Fix
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-./rom-build.sh raphael
+ln -sf vendor/aospa/build.sh build.sh
+./build.sh raphael
 
 # Exit
 exit 0
