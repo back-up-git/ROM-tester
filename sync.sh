9f12f3b2fcc6048264811aef5f68ad4951363e9b
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-27 23:12:33 +0530

    [skip ci] Update

--- sync.sh
+++ sync.sh
@@ -19,17 +19,17 @@ git clone --depth=1 $KT_LINK $KT_PATH
 rm -rf vendor/aospa
 git clone --depth=1 $VA_LINK $VA_PATH
 
-rm -rf art
-rm -rf bionic
+#rm -rf art
+#rm -rf bionic
 #rm -rf external/zlib-ng
 #rm -rf hardware/qcom-caf/sm8250/audio
 #rm -rf hardware/qcom-caf/sm8350/audio
 
 
-git clone https://github.com/pa-vayu/android_art.git -b sapphire art
-git clone https://github.com/pa-vayu/android_bionic.git -b sapphire bionic
-git clone https://github.com/AzurE-007/android_device_xiaomi_raphael-sepolicy -b ruby device/xiaomi/raphael-sepolicy
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
+#git clone https://github.com/pa-vayu/android_art.git -b sapphire art
+#git clone https://github.com/pa-vayu/android_bionic.git -b sapphire bionic
+git clone https://github.com/AzureHelper/android_device_xiaomi_raphael-sepolicy -b sapphire device/xiaomi/raphael-sepolicy
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-19.0 packages/resources/devicesettings
 
 # Exit
 exit 0
